a1915f0d27957ab2109f3d5d1c3414f7916eb8d2
Kate <ekaterina21051988@yandex.by>
2024-02-25 19:18:54 +0200

    fix: unnecessary comments have been removed

--- src/websocket_server/commands/create_room.ts
+++ src/websocket_server/commands/create_room.ts
@@ -1,8 +1,6 @@
 import { generateId } from "../../utils/generateId.js";
 import { Room, players, rooms } from "../../utils/models.js";
 import { WsWithId } from "../../utils/types.js";
-import { wsServer } from "../ws.js";
-import { WebSocket } from "ws";
 import { updateRooms } from "./update_room.js";
 
 export const createRoom = (ws: WsWithId) => {
@@ -15,15 +13,5 @@ export const createRoom = (ws: WsWithId) => {
   if (player) {
     newRoom.roomUsers.push({ name: player.name, index: player.id });
   }
-  //   const response = {
-  //     type: "create_room",
-  //     data: "",
-  //     id: 0,
-  //   };
   updateRooms(ws);
-  //   wsServer.clients.forEach((client: WebSocket) => {
-  //     if (client.readyState === WebSocket.OPEN) {
-  //       client.send(JSON.stringify(response));
-  //     }
-  //   });
 };
